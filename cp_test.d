f9463d1da991038d3577d89bf2cc1b3549c5c4ee
David Zhao <dzhao650@gmail.com>
2025-06-16 20:49:10 -0500

    feat: support autoload

--- cp_test.d
+++ cp_test.d
@@ -1,4 +1,4 @@
 #! /usr/bin/env zsh
 tc=$1
 prog=${@:2}
-[[ $# < 2 ]]&&cat $0||for each in $tc/*.in(n);do cp_test $each $prog;done
+[[ $# < 2 ]]&&{ [[ $funcstack ]]&&{ typeset -f $0 }||cat $0 }||for each in $tc/*.in(n);do cp_test $each $prog;done
